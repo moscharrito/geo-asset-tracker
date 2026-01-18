f9c01092bd4837e5c94a677e7e3b5b6df0f0fae5
moscharrito <moscharito@gmail.com>
2026-01-17 23:23:57 -0500

    nginx

---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GRAPHQL_HTTP_URL: string;
+  readonly VITE_GRAPHQL_WS_URL: string;
+  readonly VITE_MAPBOX_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
